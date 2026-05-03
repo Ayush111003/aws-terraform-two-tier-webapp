f9b3b2d075e2cc034467be3db5a720f4f56f8777
Ayush111003 <126341508+Ayush111003@users.noreply.github.com>
2026-05-03 15:49:21 -0400

    Delete acs730-group2-terraform (1)main (1)/environment directory

--- acs730-group2-terraform (1)main (1)/environment/acs730-group2-terraform/dev/network/main.tf
+++
@@ -1,9 +0,0 @@
-module "network" {
-  source          = "../../modules/network"
-  project_name    = var.project_name
-  environment     = var.environment
-  vpc_cidr        = var.vpc_cidr
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
-  azs             = var.azs
-}
